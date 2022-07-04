60178520f8281d745d1ecb54ff080bc742846fd9
kunshksingh <kunshksingh@gmail.com>
2022-07-04 17:26:05 -0400

    Fixed Raycasting Vectors and Revert to NonAllocRaycasting

--- BlindEyes/Assets/Scripts/AudioSweepManager.cs
+++ BlindEyes/Assets/Scripts/AudioSweepManager.cs
@@ -1,39 +1,41 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Threading;
+//using System.Threading;
 
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.XR.ARFoundation;
 
+
 using TMPro;
 
+
+[RequireComponent(typeof(ARRaycastManager))]
 
 public class AudioSweepManager : MonoBehaviour
 {
    
     public AudioClip clip;
 
-    int outputCounter;
+    //int outputCounter;
     float timer;
     Camera MainCamera;
     AudioSweep sweeper;
-  
 
-    protected ARRaycastManager raycastManager;
+    ARRaycastManager raycastManager;
 
 
-    private int raysToShoot;
+
+    private const int raysToShoot = 20;
   
 
     void Start()
     {
-       
+        //raycastManager = GetComponent<ARRaycastManger>();
         timer = 0.0f;
         MainCamera = Camera.main;
-        outputCounter = 0;
-        raysToShoot = 20;
+        //outputCounter = 0;
      
         
         sweeper = new AudioSweep();
@@ -42,7 +44,7 @@ public class AudioSweepManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Trigger Method Every 5 seconds
+        //Trigger Method Every 7 seconds
         timer += Time.deltaTime;
         if (timer >= 7.0f)
         {
@@ -51,50 +53,90 @@ public class AudioSweepManager : MonoBehaviour
         }
 
     }
+    void stallThreat()
+    {
+        float shortTimer = 0.0f;
+        while (shortTimer < 1.0f)
+        {
+            shortTimer += Time.deltaTime;
+        }
+    }
     //Create a raycast in the direction of the main camera
     void TriggerRaycast()
     {   
+     
         var sweeper = new AudioSweep();
 
+      
 
         float angle = 0;
-        float shortTimer = 0.0f;
+    
         
         for (int i = 0; i < raysToShoot; i++)
         {
-            /*
-             * TODO:
-             * 
-             * The math below does not disperse the rays properly from the main camera. 
-             * Check math/code to make sure the raycasts emit 360 degrees around the main camera
-             *
-             */
-            shortTimer = 0.0f;
-            while (shortTimer < 0.3f)
+            
+            
+            angle += 2 * 2 * Mathf.PI / raysToShoot;
+            float x = Mathf.Cos(angle);
+            float z = Mathf.Sin(angle);
+     
+            RaycastHit[] hit = new RaycastHit[10];
+            Debug.Log((angle*180)/(2*Mathf.PI));
+           
+            Debug.DrawLine(MainCamera.transform.position, new Vector3(transform.position.x + x, transform.position.y, transform.position.z + z)/*Direction Vector*/, Color.red, 20, false);
+                  
+            if (Physics.RaycastNonAlloc(new Ray(transform.position, new Vector3(transform.position.x + x, transform.position.y, transform.position.z + z)/*Direction Vector*/), hit, 1000) >= 1)
             {
-                shortTimer += Time.deltaTime;
-            }
-            float x = Mathf.Sin(angle);
-            float y = Mathf.Cos(angle);
-            angle += 2 * Mathf.PI / raysToShoot;
-
-            Vector3 dir = new Vector3(transform.position.x * x, transform.position.y * y, 0);
-            List<ARRaycastHit> hit = new List<ARRaycastHit>();
-            Ray directionVector = new Ray(transform.position, dir);
-
-
-            if (raycastManager.Raycast(directionVector, hit))
-            {
-                foreach(ARRaycastHit instanceHit in hit)
+                Debug.Log("Hit!");
+                foreach (RaycastHit instanceHit in hit)
                 {
+                    if (instanceHit.collider.gameObject.tag == "SoundMesh")
+                    {
+                        //Debug.Log("Hit: " + instanceHit.collider.gameObject.name);
+                        //Debug.Log("Hit: " + instanceHit.collider.gameObject.tag);
+                        //Debug.Log("Hit: " + instanceHit.collider.gameObject.transform.position);
+                        //Debug.Log("Hit: " + instanceHit.collider.gameObject.transform.rotation);
+                        //Debug.Log("Hit: " + instanceHit.collider.gameObject.transform.localScale);
+                        //Debug.Log("Hit: " + instanceHit.collider.gameObject.transform.parent.name);
+                        //Debug.Log("Hit: " + instanceHit.collider.gameObject.transform.parent.tag);
+                        //Debug.Log("Hit: " + instanceHit.collider.gameObject.transform.parent.transform.position);
+                        //Debug.Log("Hit: " + instanceHit.collider.gameObject.transform.parent.transform.rotation);
+                        //Debug.Log("Hit: " + instanceHit.collider.gameObject.transform.parent.transform.localScale);
+                        //Debug.Log("Hit: " + instanceHit.collider.gameObject.transform.parent.transform.parent.name);
+                        //Debug.Log("Hit: " + instanceHit.collider.gameObject.transform.parent.transform.parent.tag);
+                        //Debug.Log("Hit: " + instanceHit.collider.gameObject.transform.parent.transform.parent.transform.position);
+                        //Debug.Log("Hit: " + instanceHit.collider.gameObject.transform.parent.transform.parent.transform.rotation);
+                        //Debug.Log("Hit: " + instanceHit.collider.gameObject.transform.parent.transform.parent.transform.localScale);
+                        //Debug.Log("Hit: " + instanceHit.collider.gameObject.transform.parent.transform.parent.transform.parent.name);
+                        //Debug.Log("Hit: " + instanceHit.collider.gameObject.transform.parent.transform.parent.transform.parent.tag);
+                       
 
-                    Debug.Log($"Raycast hit a {instanceHit.hitType}");
-                    /*if (instanceHit.hitType & TrackableType.)
+                        sweeper.trigger(hit[0].point, clip);
+                    }
+                   
+                }
+
+            }
+
+            //List<ARRaycastHit> ARHit = new List<ARRaycastHit>();
+      
+
+            /*
+            if (raycastManager.Raycast(directionVector, ARHit))
+            {
+                foreach(ARRaycastHit instanceHit in ARHit)
+                {
+                    
+                    Debug.Log($"Raycast hit a {instanceHit.ARHitType}");
+                    if (instanceHit.hitType & TrackableType.)
                     {
                         sweeper.trigger(hit[0].point, clip);
-                    }*/
+                    }
+                    
                 }
             }
+            */
+            
         }
         /*
         outputCounter += 1;
